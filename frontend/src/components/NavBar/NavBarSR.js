2d8701df3a463735f886ca642542e5ff3ccdfd18
Ola Josh <akandetunji3@gmail.com>
2022-11-22 14:25:49 +0100

    first

--- frontend/src/components/NavBar/NavBarSR.js
+++ frontend/src/components/NavBar/NavBarSR.js
@@ -15,7 +15,7 @@ function NavBarSR(props) {
     0
   );
 
-  //TOTAL SALES 
+  //TOTAL SALES
   let TotalItemPrice = new Intl.NumberFormat("en-GB", {
     style: "currency",
     currency: "GBP",
@@ -36,118 +36,110 @@ function NavBarSR(props) {
   return (
     <div className="top-case">
       <div className="top-case-pad">
-        <div className="sign-reg">
-          <div>
-            <span>Hi! </span>
+        <div className="admin-sign-reg">
+          <div className="sign-reg">
+            <div>
+              <span>Hi! </span>
+            </div>
+            {userInfo ? (
+              <div className="user-select">
+                <ul>
+                  <li>
+                    <Link to="#">
+                      <span>{userInfo.name} </span>
+                      <i className="fa fa-caret-down"></i>
+                    </Link>
+                    <ul className="drop-list">
+                      <li>
+                        <div
+                          className="prof"
+                          onClick={() => {
+                            navigate(`/profile/${userInfo._id}`);
+                          }}
+                        >
+                          Profile
+                        </div>
+                      </li>
+                      <li>
+                        <Link to="/orderhistory">Orders </Link>
+                      </li>
+
+                      <li>
+                        <div className="user-signout">
+                          <Link to="#signout" onClick={signoutHandler}>
+                            Sign Out
+                          </Link>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div className="s-reg">
+                <Link to={"/signin"} className="sign-reg">
+                  {" "}
+                  Sign In
+                </Link>{" "}
+                <span>or</span>
+                <Link to={"/signup"} className="sign-reg">
+                  {" "}
+                  Register
+                </Link>
+              </div>
+            )}
           </div>
-          {userInfo ? (
-            <div className="user-select">
+          {userInfo && userInfo?.isAdmin && (
+            <div className="admin-select">
               <ul>
                 <li>
                   <Link to="#">
-                    <span>{userInfo.name} </span>
+                    <span>Admin</span>
                     <i className="fa fa-caret-down"></i>
                   </Link>
-                  <ul className="drop-list">
+                  <ul className="admin-drop-list">
                     <li>
-                      <div
-                        className="prof"
-                        onClick={() => {
-                          navigate(`/profile/${userInfo._id}`);
-                        }}
-                      >
-                        Profile
-                      </div>
+                      <Link to="/admin/dashboard">Dashboard</Link>
                     </li>
                     <li>
-                      <Link to="/orderhistory">Orders </Link>
+                      <Link to="/admin/products">Products</Link>
                     </li>
-
                     <li>
-                      <div className="user-signout">
-                        <Link to="#signout" onClick={signoutHandler}>
-                          Sign Out
-                        </Link>
-                      </div>
+                      <Link to="/admin/orderlist">Orders</Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/userlist">Users</Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/support">Support</Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/settings">Settings</Link>
                     </li>
                   </ul>
                 </li>
               </ul>
             </div>
-          ) : (
-            <div className="s-reg">
-              <Link to={"/signin"} className="sign-reg">
-                {" "}
-                Sign In
-              </Link>{" "}
-              <span>or</span>
-              <Link to={"/signup"} className="sign-reg">
-                {" "}
-                Register
-              </Link>
-            </div>
           )}
         </div>
-        {userInfo && userInfo?.isAdmin && (
-          <div className="admin-select">
-            <ul>
-              <li>
-                <Link to="#">
-                  <span>Admin</span>
-                  <i className="fa fa-caret-down"></i>
-                </Link>
-                <ul className="admin-drop-list">
-                  <li>
-                    <Link to="/admin/dashboard">Dashboard</Link>
-                  </li>
-                  <li>
-                    <Link to="/admin/products">Products</Link>
-                  </li>
-                  <li>
-                    <Link to="/admin/orderlist">Orders</Link>
-                  </li>
-                  <li>
-                    <Link to="/admin/userlist">Users</Link>
-                  </li>
-                  <li>
-                    <Link to="/admin/support">Support</Link>
-                  </li>
-                  <li>
-                    <Link to="/admin/settings">Settings</Link>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        )}
-        <div
-          className={
-            userInfo || userInfo?.isAdmin || userInfo?.isSeller
-              ? "islist"
-              : "list"
-          }
-        >
-          <ul>
+        <div className="list">
+          <ul className="k-list">
             <Link to="/deals">
               <li>Daily Deals</li>
             </Link>
             <li>
               {userInfo && userInfo?.isSeller ? (
                 <div className="seller-select-nav">
-                  <ul>
+                  <Link to="#">
+                    <span>Seller</span>
+                    <i className="fa fa-caret-down"></i>
+                  </Link>
+                  <ul className="seller-drop-list-nav">
                     <li>
-                      <Link to="#">
-                        <span>Seller</span>
-                        <i className="fa fa-caret-down"></i>
-                      </Link>
-                      <ul className="seller-drop-list-nav">
-                        <li>
-                          <Link to="/seller/products">Products</Link>
-                        </li>
-                        <li>
-                          <Link to="/seller/orderlist">Orders</Link>
-                        </li>
-                      </ul>
+                      <Link to="/seller/products">Products</Link>
+                    </li>
+                    <li>
+                      <Link to="/seller/orderlist">Orders</Link>
                     </li>
                   </ul>
                 </div>
